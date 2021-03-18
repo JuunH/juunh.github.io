02ba89b52f58183b3c03763c02592e0e22084380
Nicholas Ho <80541424+JuunH@users.noreply.github.com>
2021-03-18 21:44:20 +0000

    Added Javascript file. Fixed Menu Icon bug

---
+++ Javascript/scripts.js
@@ -0,0 +1,8 @@
+const menuicon = document.getElementById('menuIcon');
+const menu = document.getElementById('menu');
+
+
+menuicon.addEventListener('click', () => {
+  menuicon.classList.toggle('active');
+  menu.classList.toggle('active');
+})
