6f88a22ee72dd3b6fa938157dec80c8b1fea1428
Nicholas Ho <80541424+JuunH@users.noreply.github.com>
2021-03-22 19:32:59 +0000

    Added Gallery lightbox when any image is clicked.

--- Javascript/scripts.js
+++ Javascript/scripts.js
@@ -1,8 +1,55 @@
+//  Menu Icon Function  //
+
+
 const menuicon = document.getElementById('menuIcon');
 const menu = document.getElementById('menu');
 
 
 menuicon.addEventListener('click', () => {
+
   menuicon.classList.toggle('active');
   menu.classList.toggle('active');
+
 })
+
+
+
+//  Gallery Lightbox Function //
+
+
+const everyImg = document.getElementsByClassName('img');
+const lightbox = document.getElementById('gallerylightbox');
+const lbImage = document.getElementById('imageClicked');
+const clickedImageCaption = document.getElementById('imageCaption');
+
+
+function displayClickedImage(imageClicked) {
+  
+  image = document.getElementById(imageClicked).src;
+  var caption = document.getElementById(imageClicked).alt;
+  
+  lightbox.classList.toggle('active');
+
+  lbImage.src = image;
+  clickedImageCaption.p = caption;
+}
+
+function closeLightBox() {
+  lightbox.classList.toggle('active');
+}
+
+
+for (var i = 0 ; i < everyImg.length; i++) {
+
+  everyImg[i].setAttribute('id', 'picture'+ i);
+
+  let pictureID = 'picture' + i;
+  console.log(pictureID)
+  
+  everyImg[i].addEventListener('click' , () => {
+
+    console.log(pictureID)
+    displayClickedImage(pictureID)
+  
+  })
+}
