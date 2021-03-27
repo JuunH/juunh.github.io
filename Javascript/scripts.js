7b4ceda4b5dee1790e502adc19f097adf7f40065
Nicholas Ho <80541424+JuunH@users.noreply.github.com>
2021-03-27 00:17:46 +0000

    Added lightmode as default with darkmode toggle

--- Javascript/scripts.js
+++ Javascript/scripts.js
@@ -6,8 +6,24 @@ const menu = document.getElementById('menu');
 
 
 menuicon.addEventListener('click', () => {
-
   menuicon.classList.toggle('active');
   menu.classList.toggle('active');
+})
 
+
+//  Dark Mode Function  //
+
+const dmIcon = document.getElementById('darkmodeIcon');
+const allElements = document.getElementsByTagName('*')
+
+
+function toggleDarkmode (allElements) {
+  for (var i = 0 ; i < allElements.length; i++) {
+    allElements[i].classList.toggle('darkmode');
+  }
+}
+
+
+dmIcon.addEventListener('click', () => {
+  toggleDarkmode(allElements)
 })
