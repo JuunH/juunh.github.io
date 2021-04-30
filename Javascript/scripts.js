f527f5381609aca85dc16b5a8725205ad6f9bf0f
JUUN <NickJH.Ho@gmail.com>
2021-05-01 00:19:08 +0100

    Changed some variable names for menu icon function.

--- Javascript/scripts.js
+++ Javascript/scripts.js
@@ -1,112 +1,112 @@
 //  Menu Icon Function  //
 
 
-const menuicon = document.getElementById('menuIcon');
-const menu = document.getElementById('menuBlock');
+const menuicon = document.getElementById('menuicon');
+const options = document.getElementById('options');
 
 
-function toggleMenu() {
+function toggleOptions() {
   menuicon.classList.toggle('active');
-  menu.classList.toggle('active');
+  options.classList.toggle('active');
 }
 
 
 menuicon.addEventListener('click', () => {
-  toggleMenu()
+  toggleOptions()
 })
 
 
-//  Dark Mode Function  //
+// //  Dark Mode Function  //
 
 
-const dmIcon = document.getElementById('darkmodeIcon');
-const allElements = document.getElementsByTagName('*')
+// const dmIcon = document.getElementById('darkmodeIcon');
+// const allElements = document.getElementsByTagName('*')
 
 
-function toggleDarkmode (allElements) {
-  for (var i = 0 ; i < allElements.length; i++) {
-    allElements[i].classList.toggle('darkmode');
-  }
-}
+// function toggleDarkmode (allElements) {
+//   for (var i = 0 ; i < allElements.length; i++) {
+//     allElements[i].classList.toggle('darkmode');
+//   }
+// }
 
 
-dmIcon.addEventListener('click', () => {
-  toggleDarkmode(allElements)
-  dmIcon.classList.toggle('active')
-})
+// dmIcon.addEventListener('click', () => {
+//   toggleDarkmode(allElements)
+//   dmIcon.classList.toggle('active')
+// })
 
 
-//  Switch Content Function  //
+// //  Switch Content Function  //
 
 
-const about = document.getElementById('about')
-const projects = document.getElementById('projects')
-const designs = document.getElementById('cert')
-const gallery = document.getElementById('gallery')
-const links = document.getElementsByClassName('link')
-const returnIcon = document.getElementById('returnIcon')
+// const about = document.getElementById('about')
+// const projects = document.getElementById('projects')
+// const designs = document.getElementById('cert')
+// const gallery = document.getElementById('gallery')
+// const links = document.getElementsByClassName('link')
+// const returnIcon = document.getElementById('returnIcon')
 
-function switchElements(element) {
+// function switchElements(element) {
 
-  let activeElement = document.getElementsByClassName('show')
+//   let activeElement = document.getElementsByClassName('show')
 
-  for (var i = 0 ; i < activeElement.length; i++) {
-    activeElement[i].classList.toggle('show')
-  }
+//   for (var i = 0 ; i < activeElement.length; i++) {
+//     activeElement[i].classList.toggle('show')
+//   }
 
-  let showElement = document.getElementById(element)
-  if (showElement.id == 'home') {
-    returnIcon.classList.remove('active')
-  }
-  else {
-    returnIcon.classList.add('active')
-  }
-  showElement.classList.toggle('show')
-}
-
-
-for (var i = 0 ; i < links.length; i++) {
-
-  let pagename = links[i].getAttribute('page')
-
-  links[i].addEventListener('click', () => {
-
-    switchElements(pagename)
-
-  })
-}
+//   let showElement = document.getElementById(element)
+//   if (showElement.id == 'home') {
+//     returnIcon.classList.remove('active')
+//   }
+//   else {
+//     returnIcon.classList.add('active')
+//   }
+//   showElement.classList.toggle('show')
+// }
 
 
-//  Gallery Lightbox Function   //
+// for (var i = 0 ; i < links.length; i++) {
+
+//   let pagename = links[i].getAttribute('page')
+
+//   links[i].addEventListener('click', () => {
+
+//     switchElements(pagename)
+
+//   })
+// }
 
 
-const everyImg = document.getElementsByClassName('img');
-const lightbox = document.getElementById('gallerylightbox');
-const lbImage = document.getElementById('imageClicked');
-var scrolling;
+// //  Gallery Lightbox Function   //
 
 
-function displayClickedImage(imageClicked) {
-  image = document.getElementById(imageClicked).src;
-  lightbox.classList.toggle('active');
-  lbImage.src = image;
-}
-
-function closeLightBox() {
-  lightbox.classList.toggle('active');
-}
+// const everyImg = document.getElementsByClassName('img');
+// const lightbox = document.getElementById('gallerylightbox');
+// const lbImage = document.getElementById('imageClicked');
+// var scrolling;
 
 
-for (var i = 0 ; i < everyImg.length; i++) {
+// function displayClickedImage(imageClicked) {
+//   image = document.getElementById(imageClicked).src;
+//   lightbox.classList.toggle('active');
+//   lbImage.src = image;
+// }
 
-  everyImg[i].setAttribute('id', 'picture'+ i);
-  let pictureID = `picture${i}`;
+// function closeLightBox() {
+//   lightbox.classList.toggle('active');
+// }
 
-  everyImg[i].addEventListener('click', function () {
-      displayClickedImage(pictureID);
-  })
 
-  everyImg[i].addEventListener('touchstart', function () {
-    displayClickedImage(pictureID);
-  })
-}
+// for (var i = 0 ; i < everyImg.length; i++) {
+
+//   everyImg[i].setAttribute('id', 'picture'+ i);
+//   let pictureID = `picture${i}`;
+
+//   everyImg[i].addEventListener('click', function () {
+//       displayClickedImage(pictureID);
+//   })
+
+//   everyImg[i].addEventListener('touchstart', function () {
+//     displayClickedImage(pictureID);
+//   })
+// }
