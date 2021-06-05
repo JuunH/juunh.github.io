b36bd7432d63aed57e96b5f5654f45289765f62e
JuunH <NickJH.Ho@gmail.com>
2021-06-05 20:25:25 +0100

    Removed unused functions and added comments.

--- js/scripts.js
+++ js/scripts.js
@@ -1,59 +1,48 @@
-//  Menu Icon Function  //
+//  Menu Icon Function
 
+
+//  Get element with class menu-icon.
 const icon = document.querySelector(".menu-icon");
+//  Get element with class nav-mobile.
 const options = document.getElementById('nav-mobile');
 
 icon.addEventListener("click", () => {
+  //  Play animation to change menu icon state.
   icon.classList.toggle("clicked");
+  //  Display the mobile navigation menu.
   options.classList.toggle("active");
 });
 
 
-//  Fade in on scroll animation  //
-
-const cardArray = document.getElementsByClassName('card')
-const galleryArray = document.getElementsByClassName('imagecontainer')
-
-document.addEventListener('scroll', function(e) {
-  for (var i = 0 ; i < cardArray.length; i++) {
-    var topWindow = document.documentElement.clientHeight
-    var botWindow = document.documentElement.scrollTop + topWindow
-    if (cardArray[i].offsetTop < botWindow) {
-      cardArray[i].classList.add('fadein')
-    }
-  }
-  for (var i = 0 ; i < galleryArray.length; i++) {
-    var topWindow = document.documentElement.clientHeight
-    var botWindow = document.documentElement.scrollTop + topWindow
-    if (galleryArray[i].offsetTop < botWindow) {
-      galleryArray[i].classList.add('fadein')
-    }
-  }
-});
+// ----------------------------------------------------------
 
 
-// //  Gallery Lightbox Function   //
+//  Gallery Lightbox Function
 
 
+//  Get all images with 'img' class.
 const everyImg = document.getElementsByClassName('img');
+//  Get lightbox.
 const lightbox = document.getElementById('gallerylightbox');
+//  Get element that will contain the clicked image.
 const lbImage = document.getElementById('imageClicked');
-var scrolling;
-
 
 function displayClickedImage(imageClicked) {
+  //  Set image to the clicked image's Picture ID.
   image = document.getElementById(imageClicked).src;
+  //  Show lightbox.
   lightbox.classList.toggle('active');
+  //  Set lightbox image container to contain the clicked image.
   lbImage.src = image;
 }
-
 function closeLightBox() {
+  //  Hide the lightbox.
   lightbox.classList.toggle('active');
 }
 
-
+//  For every image in everyImg array.
 for (var i = 0 ; i < everyImg.length; i++) {
-
+  //  Set an ID to the image in position number i.
   everyImg[i].setAttribute('id', 'picture'+ i);
   let pictureID = `picture${i}`;
 
