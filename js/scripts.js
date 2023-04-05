655f6bb212401c150817e4378501d533b26f710d
juunh <nickjh.ho@gmail.com>
2023-04-05 02:44:41 +0100

    Revised the page switch code without refreshing

--- js/scripts.js
+++ js/scripts.js
@@ -5,53 +5,65 @@
 
 
 const icon = document.querySelector(".menu-icon");
-const options = document.getElementById('nav-mobile');
+const nav_mobile = document.getElementById('nav-mobile');
 
 icon.addEventListener("click", () => {
   icon.classList.toggle("clicked");
-  options.classList.toggle("show");
+  nav_mobile.classList.toggle("show");
 });
 
 
 // ----------------------------------------------------------
 
 
-//  Change page function
+// Change Page Function
 
 
-const links = document.getElementsByClassName('link')
-console.log(links)
+function loadPage(url, selector) {
+  fetch(url)
+    .then(response => response.text())
+    .then(html => {
+      // load specific element from selected html page
+      const newContent = document.createRange().createContextualFragment(html).querySelector(selector);
+      // Select node 'main' from current page
+      const currentContent = document.querySelector(selector);
 
+      newContent.classList.add('hidden');
+      currentContent.classList.add('hidden');
 
-function switchElements(element) {
-  let activeElement = document.getElementsByClassName('active')
-  for (var i = 0 ; i < activeElement.length; i++) {
-    activeElement[i].classList.toggle('active')
-  }
-  icon.classList.toggle("clicked");
-  options.classList.toggle("show");
-  let showElement = document.getElementById(element)
-  showElement.classList.toggle('active')
+      setTimeout(() => {
+        currentContent.innerHTML = newContent.innerHTML;
+        
+        // Add link event listener to any newly loaded link elements
+        var links = currentContent.querySelectorAll('a');
+        addLinkListener(links);
+
+        currentContent.classList.remove('hidden');
+      }, 250); // 250ms timeout
+    })
+
+    .catch(error => console.error(error));
 }
 
-//  Change page without displaying mobile nav menu.
-function hyperlinkSwitch(element) {
-  let activeElement = document.getElementsByClassName('active')
-  for (var i = 0 ; i < activeElement.length; i++) {
-    activeElement[i].classList.toggle('active')
-  }
-  let showElement = document.getElementById(element)
-  showElement.classList.toggle('active')
+
+function addLinkListener(links) {
+  links.forEach(link => {
+    link.addEventListener('click', event => {
+      event.preventDefault(); // Disable loading the linked page
+
+      loadPage(link.href, 'main');
+  
+      // If not already, revert menu icon and mobile menu back to original state
+      if (nav_mobile.classList.contains('show') && icon.classList.contains('clicked')) {
+        icon.classList.toggle('clicked');
+        nav_mobile.classList.toggle('show');
+      }
+    });
+  });
 }
 
-
-for (var i = 0 ; i < links.length; i++) {
-  let pagename = links[i].getAttribute('page')
-  links[i].addEventListener('click', () => {
-    switchElements(pagename)
-  })
-}
-
+var links = document.querySelectorAll('a');
+addLinkListener(links);
 
 
 // ----------------------------------------------------------
@@ -59,32 +71,28 @@ for (var i = 0 ; i < links.length; i++) {
 
 //  Gallery Lightbox Function
 
-
-const everyImg = document.getElementsByClassName('img');
-const lightbox = document.getElementById('gallerylightbox');
-const lbImage = document.getElementById('imageClicked');
-
-
-function displayClickedImage(imageClicked) {
-  image = document.getElementById(imageClicked).src;
-  lightbox.classList.toggle('active');
-  lbImage.src = image;
-}
+// const everyImg = document.getElementsByClassName('img');
+// const lightbox = document.getElementById('gallerylightbox');
+// const lbImage = document.getElementById('imageClicked');
 
 
-function closeLightBox() {
-  lightbox.classList.toggle('active');
-}
+// function displayClickedImage(imageClicked) {
+//   image = document.getElementById(imageClicked).src;
+//   lightbox.classList.toggle('active');
+//   lbImage.src = image;
+// }
 
 
-for (var i = 0 ; i < everyImg.length; i++) {
-  everyImg[i].setAttribute('id', 'picture'+ i);
-  let pictureID = `picture${i}`;
-  everyImg[i].addEventListener('click', function () {
-      displayClickedImage(pictureID);
-  })
-}
+// function closeLightBox() {
+//   lightbox.classList.toggle('active');
+// }
 
 
-// ----------------------------------------------------------
+// for (var i = 0 ; i < everyImg.length; i++) {
+//   everyImg[i].setAttribute('id', 'picture'+ i);
+//   let pictureID = `picture${i}`;
+//   everyImg[i].addEventListener('click', function () {
+//       displayClickedImage(pictureID);
+//   })
+// }
 
